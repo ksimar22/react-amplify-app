987d57b37ab7410194134120e2d8cd8681e524ae
Simar <sskhataytu@gmail.com>
2023-04-09 21:53:22 -0700

    hope it works

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route exact path='/about'>
-            <About />
-          </Route>
-          <Route exact path='/portfolio'>
-            <Portfolio />
-          </Route>
-          <Route exact path='/contact'>
-            <Contact />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
   );
 }
 
