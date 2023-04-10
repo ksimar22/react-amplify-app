a2fbf746dade3a0d077fb69dbc202f46f104c0d3
Simar <sskhataytu@gmail.com>
2023-04-09 21:19:29 -0700

    ...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="https://nosidebar.com/wp-content/uploads/i-love-you.jpg" />
-        <h1>Hello Babe I love you a lot</h1>
+        <h1>Hello Babe I love you a lot...</h1>
       </header>
     </div>
   );
