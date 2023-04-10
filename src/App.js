be612a587ce6bf9c88d67ba7465e0c473cc8ab66
Simar <sskhataytu@gmail.com>
2023-04-09 21:44:20 -0700

    Port

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://nosidebar.com/wp-content/uploads/i-love-you.jpg" />
-        <h1>Hello Babe I love you a lot...</h1>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/about'>
+            <About />
+          </Route>
+          <Route exact path='/portfolio'>
+            <Portfolio />
+          </Route>
+          <Route exact path='/contact'>
+            <Contact />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
