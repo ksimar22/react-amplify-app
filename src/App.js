44c1589117bb2a22449286216fa373541badee34
Simar <sskhataytu@gmail.com>
2023-04-09 21:33:23 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://nosidebar.com/wp-content/uploads/i-love-you.jpg" />
-        <h1>Hello Babe I love you a lot...</h1>
-      </header>
-    </div>
-  );
+ // Smooth scrolling for navigation links
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
+
+// Toggle mobile menu
+$('.menu-toggle').click(function() {
+  $('nav').toggleClass('open');
+});
+
+// Typed.js for animated text
+var typed = new Typed('.typed-text', {
+  strings: ['Web Developer', 'Designer', 'Problem Solver'],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true
+});
+
+// Particle.js for background animation
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('Particles loaded!');
+});
+
 }
 
 export default App;
