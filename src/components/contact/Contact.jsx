dab1c5a525c7c6f6927f801bd6a4d55165aee8a0
Simar <sskhataytu@gmail.com>
2023-04-23 15:49:53 -0700

    ipl

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h2>Contact Me</h2>
+
+      <div className="continer contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>sskhataytu@gmail.com</h5>
+            <a href="sskhataytu@gmail.com">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Phone Number</h4>
+            <h5>425.977.9044</h5>
+          </article>
+        </div>
+
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+      </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import './services.css'
+import {TbPoint} from 'react-icons/tb'
 
 const Services = () => {
   return (
-    <section id='services'>Certification</section>
+    <section id='services'>
+      
+     <h2>Certifications</h2>
+
+     <div className="container services__container">
+      <article className='service'>
+        <div className="service__head">
+          <h3>IBM Fullstack Cloud Developer</h3>
+        </div>
+
+        <ul className='service__list'>
+          <li>
+            <TbPoint className='service__list-icon' />
+            <p>I did this</p>
+          </li>
+          <li>
+            <TbPoint className='service__list-icon' />
+            <p>I did this</p>
+          </li>
+          <li>
+            <TbPoint className='service__list-icon' />
+            <p>I did this</p>
+          </li>
+          <li>
+            <TbPoint className='service__list-icon' />
+            <p>I did this</p>
+          </li>
+          </ul>
+      </article>
+{/*END OF IBM CERTIFICATE */}
+
+      <article className="service">
+        <div className="service__head">
+          <h3>AWS Cloud Practioner</h3>
+        </div>
+
+        <ul className='service__list'>
+          <li>
+            <TbPoint className='service__list-icon' />
+            <p>I did this</p>
+          </li>
+          <li>
+            <TbPoint className='service__list-icon' />
+            <p>I did this</p>
+          </li>
+          <li>
+            <TbPoint className='service__list-icon' />
+            <p>I did this</p>
+          </li>
+          <li>
+            <TbPoint className='service__list-icon' />
+            <p>I did this</p>
+          </li>
+          </ul>
+      </article>
+{/*END OF AWS CERTIFICATE */}
+
+     </div>
+      </section>
   )
 }
 
